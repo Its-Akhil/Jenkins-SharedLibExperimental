a4175b124b1cc46365ed46ab400d2a94a1961a65
Akhil <74135019+Its-Akhil@users.noreply.github.com>
2025-12-26 23:55:35 +0530

    Comment out Dockerfile cat command

--- vars/build_docker.groovy
+++ vars/build_docker.groovy
@@ -1,6 +1,6 @@
 def call(){
                 sh "touch .dockerignore"
                 sh 'echo CMD \'["gunicorn", "notesapp.wsgi:application", "--bind", "0.0.0.0:8000", "--workers=3"]\' >> Dockerfile'
-                sh "cat Dockerfile"
+                // sh "cat Dockerfile"
                 sh "docker buildx build --quiet -t notes-app:latest ."
 }
