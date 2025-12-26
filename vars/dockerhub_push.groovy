baf370486837841fc63d6a7b133b23d23f4bbe10
Akhil <74135019+Its-Akhil@users.noreply.github.com>
2025-12-26 23:48:58 +0530

    Refactor dockerhub_push to accept parameters

--- vars/dockerhub_push.groovy
+++ vars/dockerhub_push.groovy
@@ -1,12 +1,12 @@
-def call(){
+def call( String credName, String repoName, String tag){
                   withCredentials(
                     [usernamePassword(
-                        credentialsId:'DockerHub',
+                        credentialsId: '${credName}',
                         passwordVariable: 'varForPass',
                         usernameVariable: 'varForUname'
                     )]){
                         sh 'docker login -u ${varForUname} -p ${varForPass}'
-                        sh 'docker image tag notes-app:latest ${varForUname}/jenkins-notes-app:latest'
-                        sh 'docker push ${varForUname}/jenkins-notes-app:latest'
+                        sh 'docker image tag ${repoName}:${tag} ${varForUname}/${repoName}:${tag}'
+                        sh 'docker push ${varForUname}/${repoName}:${tag}'
                     }
 }
