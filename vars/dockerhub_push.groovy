051b8220c501084b72b6dcdb32e46e6337366cc9
Akhil <74135019+Its-Akhil@users.noreply.github.com>
2025-12-26 23:57:51 +0530

    Fix credentialsId interpolation in dockerhub_push

--- vars/dockerhub_push.groovy
+++ vars/dockerhub_push.groovy
@@ -1,7 +1,7 @@
 def call( String credName, String repoName, String tag){
                   withCredentials(
                     [usernamePassword(
-                        credentialsId: '${credName}',
+                        credentialsId: credName,
                         passwordVariable: 'varForPass',
                         usernameVariable: 'varForUname'
                     )]){
