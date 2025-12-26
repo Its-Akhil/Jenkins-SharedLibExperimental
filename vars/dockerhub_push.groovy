5ce698567c5b36425ab30d6c2710815520445ffc
Akhil <74135019+Its-Akhil@users.noreply.github.com>
2025-12-27 00:03:35 +0530

    Fix string interpolation in dockerhub_push.groovy

--- vars/dockerhub_push.groovy
+++ vars/dockerhub_push.groovy
@@ -1,12 +1,12 @@
 def call( String credName, String repoName, String tag){
                   withCredentials(
                     [usernamePassword(
-                        credentialsId: credName,
+                        credentialsId: "${credName}",
                         passwordVariable: 'varForPass',
                         usernameVariable: 'varForUname'
                     )]){
-                        sh 'docker login -u ${varForUname} -p ${varForPass}'
-                        sh 'docker image tag ${repoName}:${tag} ${varForUname}/${repoName}:${tag}'
-                        sh 'docker push ${varForUname}/${repoName}:${tag}'
+                        sh "docker login -u ${varForUname} -p ${varForPass}"
+                        sh "docker image tag ${repoName}:${tag} ${varForUname}/${repoName}:${tag}"
+                        sh "docker push ${varForUname}/${repoName}:${tag}"
                     }
 }
