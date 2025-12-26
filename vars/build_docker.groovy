6152af49a8636ef5e1ee46bd5a766dad74f2cef6
Akhil <74135019+Its-Akhil@users.noreply.github.com>
2025-12-27 00:06:56 +0530

    Change Docker image tag from notes-app to jenkins-notes-app

--- vars/build_docker.groovy
+++ vars/build_docker.groovy
@@ -2,5 +2,5 @@ def call(){
                 sh "touch .dockerignore"
                 sh 'echo CMD \'["gunicorn", "notesapp.wsgi:application", "--bind", "0.0.0.0:8000", "--workers=3"]\' >> Dockerfile'
                 // sh "cat Dockerfile"
-                sh "docker buildx build --quiet -t notes-app:latest ."
+                sh "docker buildx build --quiet -t jenkins-notes-app:latest ."
 }
